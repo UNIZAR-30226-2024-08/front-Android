f4fe79623fc4837c7ab6ce5b68aef00ae72b67ac
Lucía <lucia.moralesrosa99@gmail.com>
2024-03-05 16:26:40 +0100

    funcion getHolaMundo
    No es funcional porque hace falta finalizar la parte del backend

--- frontend-lotus/src/app/app.component.ts
+++ frontend-lotus/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MiApiService } from './mi-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend-lotus';
+
+  constructor(private miApiService: MiApiService) { }
+  ngOnInit(): void {
+    this.miApiService.getData().subscribe({
+      next: (data: any) => {
+        // Respuesta exitosa de la API
+        console.log(data);
+      },
+      error: (error: any) => {
+        // Manejar errores
+        console.error('Error en la solicitud HTTP: ', error);
+      }
+    });
+  }
 }
