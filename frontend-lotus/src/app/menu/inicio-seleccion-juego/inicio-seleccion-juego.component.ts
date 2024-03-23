4f8b4c255522a984e8be3a2d25a73e6248dc8a04
Lucía <lucia.moralesrosa99@gmail.com>
2024-03-23 12:40:17 +0100

    Cabecera
    Intento de cabecera compartida en otro módulo. Hay algun error

--- frontend-lotus/src/app/menu/inicio-seleccion-juego/inicio-seleccion-juego.component.ts
+++ frontend-lotus/src/app/menu/inicio-seleccion-juego/inicio-seleccion-juego.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CabeceraComponent } from '../../shared/cabecera/cabecera.component';
 
 @Component({
   selector: 'app-inicio-seleccion-juego',
   standalone: true,
-  imports: [],
+  imports: [CabeceraComponent],
   templateUrl: './inicio-seleccion-juego.component.html',
   styleUrl: './inicio-seleccion-juego.component.css'
 })
