21d6d758aa9608e031efbda9869e088b20cd45a1
Lucía <lucia.moralesrosa99@gmail.com>
2024-03-23 11:25:45 +0100

    Primera versión sin funcionalidades
    Solo se ha implementado el diseño, faltan las opciones de navegación entre otras

---
+++ frontend-lotus/src/app/inicio-seleccion-juego/inicio-seleccion-juego.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-seleccion-juego',
+  standalone: true,
+  imports: [],
+  templateUrl: './inicio-seleccion-juego.component.html',
+  styleUrl: './inicio-seleccion-juego.component.css'
+})
+export class InicioSeleccionJuegoComponent {
+
+}
