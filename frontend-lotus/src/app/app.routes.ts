75178f5933d6cc74a779fb6ed0e4825400293207
MarcosGarciaOrtega <844419@unizar.es>
2024-04-08 20:54:37 +0200

    Login con google
    Funciona el login con google, tutorial y pagina de google oficial en los archivos .TS del login de google

--- frontend-lotus/src/app/app.routes.ts
+++ frontend-lotus/src/app/app.routes.ts
@@ -1,19 +1,13 @@
 import { Routes } from '@angular/router';
-import { PerfilPersonalComponent } from './menu/perfil-personal/perfil-personal.component';
 
 export const routes: Routes = [
 {
     path: '',
-    component: PerfilPersonalComponent
-
-},
-{
-    path: 'login',
     loadChildren: ()=> import('./login/login.routes').then(m => m.LOGIN_ROUTES)
 },
 
 {
-    path: '',
+    path: 'menu',
     loadChildren: ()=> import('./menu/menu.routes').then(m => m.MENU_ROUTES)
 },
 
---
+++ frontend-lotus/src/app/login/reg-con-google/reg-con-google.component.ts
@@ -0,0 +1,49 @@
+//https://developers.google.com/identity/gsi/web/guides/overview?hl=es-419 -- Pagina web
+//https://www.youtube.com/watch?v=EO-U01u9vFQ -- Video
+
+declare var google: any;
+
+import { Component, OnInit } from '@angular/core';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-reg-con-google',
+  standalone: true,
+  imports: [],
+  templateUrl: './reg-con-google.component.html',
+  styleUrl: './reg-con-google.component.css'
+})
+export class RegConGoogleComponent implements OnInit{
+  private router = inject(Router);
+  ngOnInit(): void {
+    google.accounts.id.initialize({
+      client_id: '287725710191-56khg274chrdgkt1o8idkhl5g42o8522.apps.googleusercontent.com',
+      callback: (resp: any) => this.controlarLogin(resp)
+    });
+
+      google.accounts.id.renderButton(document.getElementById("google-btn"), {
+        theme: 'filled_blue',
+        size: 'large',
+        with: 350,
+        shape: 'circle'
+      });
+  }
+
+  private decodificarToken(token: string){
+    return JSON.parse(atob(token.split('.')[1]));
+  }
+
+  controlarLogin(respuesta: any){
+    if(respuesta){
+      //Hay que decodificar el token
+      const loadToken = this.decodificarToken(respuesta.credential);
+      //Guardar el token en el local storage
+      sessionStorage.setItem("loggedUser", JSON.stringify(loadToken));
+      this.router.navigate(['/menu'])
+
+    }
+  }
+}
+
+
