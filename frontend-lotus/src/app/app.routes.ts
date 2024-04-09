432a4dae03f41c5b3e7fd871a22adf7141a0e3b1
MarcosGarciaOrtega <844419@unizar.es>
2024-04-09 11:51:17 +0200

    Cabecera_corregida

--- frontend-lotus/src/app/app.routes.ts
+++ frontend-lotus/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
+import { PerfilPersonalComponent } from './menu/perfil-personal/perfil-personal.component';
 
 export const routes: Routes = [
 {
     path: '',
+    component: PerfilPersonalComponent
+},
+{
+    path: 'login',
     loadChildren: ()=> import('./login/login.routes').then(m => m.LOGIN_ROUTES)
 },
 
