e99d3902f7f00a451239b998883cece577bd12f1
aroaaredondo <851769@unizar.es>
2024-04-19 17:26:48 +0200

    Todas las rutas correguidas

--- frontend-lotus/src/app/menu/menu.routes.ts
+++ frontend-lotus/src/app/menu/menu.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { InicioSeleccionJuegoComponent } from "./inicio-seleccion-juego/inicio-seleccion-juego.component";
 import { BjSeleccionModoComponent } from "./bj-seleccion-modo/bj-seleccion-modo.component";
 import { PokerSeleccionModoComponent } from "./poker-seleccion-modo/poker-seleccion-modo.component";
+import { PerfilPersonalComponent } from "./perfil-personal/perfil-personal.component";
 
 export const MENU_ROUTES: Routes = [
     {
@@ -18,5 +19,10 @@ export const MENU_ROUTES: Routes = [
         path: 'poker_seleccion',
         title: 'poker_seleccion-juego',
         component: PokerSeleccionModoComponent
+    },
+    {
+        path: 'perfil_personal',
+        title: 'perfil_personal',
+        component: PerfilPersonalComponent
     }
 ];
---
+++ frontend-lotus/src/app/shared/shared.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from "@angular/router";
+import { PerfilPersonalComponent } from "../menu/perfil-personal/perfil-personal.component";
+export const MENU_ROUTES: Routes = [
+    {
+        path: '/menu/perfil_personal'
+    }
+];
