2645882d1b2464f817de2b3de5064493414a3f2b
aroaaredondo <851769@unizar.es>
2024-04-19 13:47:05 +0200

    html arreglado

--- frontend-lotus/src/app/app.routes.ts
+++ frontend-lotus/src/app/app.routes.ts
@@ -1,13 +1,10 @@
 import { Routes } from '@angular/router';
 import { PerfilPersonalComponent } from './menu/perfil-personal/perfil-personal.component';
+import { LoginComponent } from './login/login/login.component';
 
 export const routes: Routes = [
 {
     path: '',
-    component: PerfilPersonalComponent
-},
-{
-    path: 'login',
     loadChildren: ()=> import('./login/login.routes').then(m => m.LOGIN_ROUTES)
 },
 
