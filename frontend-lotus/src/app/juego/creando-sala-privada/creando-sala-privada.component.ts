5a308da7ca0136c3963d605e1c6f2fd62be2ea75
aroaaredondo <851769@unizar.es>
2024-04-21 19:17:20 +0200

    Rutas
    se han añadido las rutas en el pker para poder crear una sala privada o unisera, ademas de una variable de servicio para saber si se esta creando una partida tipo poker o tipo blackJack

--- frontend-lotus/src/app/juego/creando-sala-privada/creando-sala-privada.component.ts
+++ frontend-lotus/src/app/juego/creando-sala-privada/creando-sala-privada.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { TipoJuegoService } from '../../api/tipo-juego.service';
 @Component({
   selector: 'app-creando-sala-privada',
   standalone: true,
@@ -8,11 +9,13 @@ import { Router } from '@angular/router';
   styleUrl: './creando-sala-privada.component.css'
 })
 export class CreandoSalaPrivadaComponent {
-   constructor(private router: Router) { }
+  
+  constructor(private router: Router,private tipo :TipoJuegoService) { }
 
   ngOnInit(): void {
     setTimeout(() => {
       this.router.navigate(['/juego/crear-sala-privada']); // Redirige a la ruta /juego/crear-sala-privada
     }, 4000); // 4000 milisegundos = 4 segundos
+    this.tipo.tipoJuego$.subscribe(data => {console.log(`El valor de la variable cambio a: ${data}`);});
   }
 }
--- frontend-lotus/src/app/menu/bj-seleccion-modo/bj-seleccion-modo.component.ts
+++ frontend-lotus/src/app/menu/bj-seleccion-modo/bj-seleccion-modo.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CabeceraComponent } from '../../shared/cabecera/cabecera.component';
 import { RouterModule } from '@angular/router';
+import { CreandoSalaPrivadaComponent } from '../../juego/creando-sala-privada/creando-sala-privada.component';
+import { TipoJuegoService } from '../../api/tipo-juego.service';
 
 @Component({
   selector: 'app-bj-seleccion-modo',
   standalone: true,
-  imports: [CabeceraComponent,RouterModule],
+  imports: [CabeceraComponent, RouterModule],
   templateUrl: './bj-seleccion-modo.component.html',
   styleUrl: './bj-seleccion-modo.component.css'
 })
 export class BjSeleccionModoComponent {
 
+  constructor(private tipo: TipoJuegoService) {}
+
+  ngOnInit(): void {
+    // Your code here
+    this.tipo.tipoJuego$.next("blackjack");
+  
+  }
 }
