a9c0d9a637a252d7087911200c00059b2739aade
aroaaredondo <851769@unizar.es>
2024-04-21 19:59:27 +0200

    Manejo de rutas al unirse a una sala privada
    Se ha realizado una primera versión para el manejo de las rutas solo que se ha probado con otras otras rutas ya que no se encuentran aun las definitivas

--- frontend-lotus/src/app/juego/unirse-sala-privada/unirse-sala-privada.component.ts
+++ frontend-lotus/src/app/juego/unirse-sala-privada/unirse-sala-privada.component.ts
@@ -1,20 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import {  FormControl, FormGroup,ReactiveFormsModule,Validators } from '@angular/forms';
-import e from 'express';
+import { Router } from '@angular/router';
 import { debounceTime } from 'rxjs';
+import { TipoJuegoService } from '../../api/tipo-juego.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-unirse-sala-privada',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule,CommonModule,RouterModule],
   templateUrl: './unirse-sala-privada.component.html',
   styleUrl: './unirse-sala-privada.component.css'
 })
 export class UnirseSalaPrivadaComponent implements OnInit{
   form !: FormGroup;
-
-  constructor() {
+  tipoJuego !: string;
+  constructor(private tipo :TipoJuegoService,private router: Router) {
     this.buildForm();
   }
 
@@ -23,6 +25,10 @@ export class UnirseSalaPrivadaComponent implements OnInit{
       debounceTime(500)
     ).subscribe((value: any) => {
       console.log(value);
+    });
+    this.tipo.tipoJuego$.subscribe(data => {
+      console.log(`El valor de la variable cambio a: ${data}`);
+      this.tipoJuego = data as string;
     });
   }
 
@@ -34,12 +40,20 @@ export class UnirseSalaPrivadaComponent implements OnInit{
   
   save(event: Event) {
     event.preventDefault();
+    /*Cambiar a las rutas de juego correspondientes*/
     if (this.form.valid) {
       const value = this.form.value.codigo;
       console.log(value);
+      if(this.tipoJuego == "poker"){
+        console.log('Uniendo a sala de poker');
+        this.router.navigate(['/menu/poker_seleccion']);
+      } else if(this.tipoJuego == "blackjack"){{
+        this.router.navigate(['/menu/bj_seleccion']);
+      }
     } else {
       console.log('Formulario no valido');
-    }
-  }
+    }  
+  } 
 
+  }
 }
