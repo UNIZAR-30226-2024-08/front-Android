d2ea1fcca147db2c8b67d702ef53eb68b1517a62
aroaaredondo <851769@unizar.es>
2024-04-21 14:29:36 +0200

    pantalla de carga creada

--- frontend-lotus/src/app/juego/juego.routes.ts
+++ frontend-lotus/src/app/juego/juego.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from "@angular/router";
+import { CrearSalaPrivadaComponent } from "./crear-sala-privada/crear-sala-privada.component";
 
-export const JUEGO_ROUTES: Routes = [];
+export const JUEGO_ROUTES: Routes = [
+    { 
+        path: 'crear-sala-privada',
+        component: CrearSalaPrivadaComponent
+    }
+];
