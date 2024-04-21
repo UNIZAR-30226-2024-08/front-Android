cb779873746155cf46704b9219792542f0281dc0
aroaaredondo <851769@unizar.es>
2024-04-21 12:56:51 +0200

    Mensajes de error de la apuesta
    Anteriormente dejaba poner valores negativos o cero y ahora validamos que la apuesta minima sea x y que el campo sea requerido

--- frontend-lotus/src/app/juego/bj-one-player/bj-one-player.component.ts
+++ frontend-lotus/src/app/juego/bj-one-player/bj-one-player.component.ts
@@ -1,32 +1,66 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormControlName, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { log } from 'console';
+import { debounceTime } from 'rxjs';
 @Component({
   selector: 'app-bj-one-player',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './bj-one-player.component.html',
   styleUrl: './bj-one-player.component.css'
 })
-export class BjOnePlayerComponent{
+export class BjOnePlayerComponent implements OnInit{
+
+  form!: FormGroup;
+
+  constructor(){
+    this.buildForm();
+  }
+  
+  ngOnInit(): void {
+    this.form.valueChanges.pipe(
+      debounceTime(500)
+    ).subscribe((value: any) => {
+      console.log(value);
+    });
+  }
+  
+
+  private buildForm(){
+    this.form = new FormGroup({
+      apuesta: new FormControl('',[Validators.required,Validators.min(10)])
+    });
+  }
+
+
 
   apuesta : number = 0;
   mostrarApuesta : boolean = true;
 
-  checkoutFrom = this.fb.group({
-    apuesta : FormGroup
-  });
+  // checkoutFrom = this.fb.group({
+  //   apuesta : FormGroup
+  // });
 
-  constructor(private fb: FormBuilder) { }
+  // constructor(private fb: FormBuilder) { }
   
-  onSubmit(): void {
-    /*mensaje de apuesta realizada*/
-    this.checkoutFrom.value.apuesta = this.checkoutFrom.value.apuesta;
-    //console.log(this.checkoutFrom.value.apuesta);
-    this.apuesta = Number(this.checkoutFrom.value.apuesta);
-    //console.log(this.apuesta);
-    this.checkoutFrom.reset();
-    this.mostrarApuesta = false;   
+  // onSubmit(): void {
+  //   /*mensaje de apuesta realizada*/
+  //   this.checkoutFrom.value.apuesta = this.checkoutFrom.value.apuesta;
+  //   //console.log(this.checkoutFrom.value.apuesta);
+  //   this.apuesta = Number(this.checkoutFrom.value.apuesta);
+  //   //console.log(this.apuesta);
+  //   this.checkoutFrom.reset();
+  //   this.mostrarApuesta = false;   
+  // }
+  save(event : Event){
+    
+    event.preventDefault();
+    if(this.form.valid){
+      this.mostrarApuesta = false;
+      this.apuesta = Number(this.form.value.apuesta);
+    }
+
   }
 
   
