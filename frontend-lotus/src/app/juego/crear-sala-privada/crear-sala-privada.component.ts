fdd6cfc61286695259cf65f21d3e3fd1ba156f72
aroaaredondo <851769@unizar.es>
2024-04-21 13:49:54 +0200

    componente creado

---
+++ frontend-lotus/src/app/juego/crear-sala-privada/crear-sala-privada.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-sala-privada',
+  standalone: true,
+  imports: [],
+  templateUrl: './crear-sala-privada.component.html',
+  styleUrl: './crear-sala-privada.component.css'
+})
+export class CrearSalaPrivadaComponent {
+
+}
