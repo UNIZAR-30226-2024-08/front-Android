4bb0286f9b6d52ebcba23890a9f423985cfd003b
Lucía <lucia.moralesrosa99@gmail.com>
2024-05-03 12:32:33 +0200

    Arreglo de redirecciones para un jugador y multijugador

--- frontend-lotus/src/app/juego/juego.routes.ts
+++ frontend-lotus/src/app/juego/juego.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from "@angular/router";
 import { CrearSalaPrivadaComponent } from "./crear-sala-privada/crear-sala-privada.component";
 import { CreandoSalaPrivadaComponent } from "./creando-sala-privada/creando-sala-privada.component";
 import { UnirseSalaPrivadaComponent } from "./unirse-sala-privada/unirse-sala-privada.component";
+import { BjMultiplayerComponent } from "./bj-multiplayer/bj-multiplayer.component";
+import { BjOnePlayerComponent } from "./bj-one-player/bj-one-player.component";
 
 export const JUEGO_ROUTES: Routes = [
     { 
@@ -15,5 +17,13 @@ export const JUEGO_ROUTES: Routes = [
     {
         path:'unirse-sala-privada',
         component: UnirseSalaPrivadaComponent
+    },
+    {
+        path:'bj-multiplayer',
+        component: BjMultiplayerComponent
+    },
+    {
+        path:'bj-one-player',
+        component: BjOnePlayerComponent
     }
 ];
